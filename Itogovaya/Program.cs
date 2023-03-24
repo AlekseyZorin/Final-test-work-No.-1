1fd708dc088dcabbae81ab09b3cd6c89ec0867d4
Aleksey Zorin <zorin.95@mail.ru>
2023-03-24 21:49:06 +0300

    Final code

--- Itogovaya/Program.cs
+++ Itogovaya/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Enter the length for the initial array: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+if (size > 0)
+{
+    string[] InitialStringArray = new string[size];
+    
+    for (int i = 0; i < InitialStringArray.Length; i++)
+    {
+        Console.Write($"Enter string {i + 1}: ");
+        InitialStringArray[i] = Console.ReadLine();
+    }
+
+    string[] ResultingStringArray = InitialStringArray.Where(s => s.Length <= 3).ToArray();
+
+    if (ResultingStringArray.Length > 0)
+    {
+        Console.Write("Result array -> [");
+        for (int i = 0; i < ResultingStringArray.Length; i++)
+        {
+            Console.Write($"\"{ResultingStringArray[i]}\"");
+            if (i != ResultingStringArray.Length - 1) Console.Write(", ");
+        }
+        Console.WriteLine("]");
+    }
+    else Console.WriteLine("This array contains no strings containing three or less characters.");
+}
+else Console.WriteLine("The length of the array must be greater than zero.");
